4043cc8d084afcf3ae573c73172e7951cd3afbe2
damir224 <damir224@bk.ru>
2021-03-19 20:19:10 +0500

    sm changes

--- serve.js
+++ serve.js
@@ -7,11 +7,6 @@ const app = express();
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
-
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`app is listening on port: ${port}`);
